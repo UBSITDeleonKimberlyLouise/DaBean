279bc4b815b291ba6a7bad18abf2e196ea13cd12
de leon_k <de leon_k@ca01.ub.lan>
2026-04-20 13:58:34 +0800

    components added

---
+++ src/app/review-card/review-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-card',
+  imports: [],
+  templateUrl: './review-card.html',
+  styleUrl: './review-card.css',
+})
+export class ReviewCard {}
